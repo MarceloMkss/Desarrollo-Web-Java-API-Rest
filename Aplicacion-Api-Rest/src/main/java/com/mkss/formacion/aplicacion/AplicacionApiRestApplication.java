b2ebc86c9c7c47e8385fed359f852cf376dbde79
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-02 20:15:42 +0100

    Creacion del Proyecto con Spring usando jpa con API REST

---
+++ Aplicacion-Api-Rest/src/main/java/com/mkss/formacion/aplicacion/AplicacionApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.mkss.formacion.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionApiRestApplication.class, args);
+	}
+
+}
