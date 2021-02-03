7f42ba53d4f0a3bf7db00c425cbb89d307c1569c
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-03 10:45:33 +0100

    crear ClienteDaoTreeMap que implementara el Dao, para usar sus metodos.

---
+++ servidorrestservlets/src/com/mkss/formacion/rest/servidoresrestservlets/repository/ClienteDaoTreeMap.java
@@ -0,0 +1,54 @@
+package com.mkss.formacion.rest.servidoresrestservlets.repository;
+
+import java.time.LocalDate;
+import java.util.TreeMap;
+
+import com.mkss.formacion.rest.servidoresrestservlets.controlador.entidades.Cliente;
+
+public class ClienteDaoTreeMap implements Dao<Cliente> {
+	
+	private static TreeMap<Long, Cliente> clientes = new TreeMap<>();
+
+	static {
+		clientes.put(1L, new Cliente(1L, "Javier", LocalDate.now()));
+		clientes.put(2L, new Cliente(2L, "Pepe", LocalDate.now()));
+	}
+
+	@Override
+	public Iterable<Cliente> obtenerTodos() {
+		
+		return clientes.values();
+	}
+
+	@Override
+	public Cliente obtenerPorId(Long id) {
+		return clientes.get(id);
+	}
+
+	@Override
+	public Cliente insertar(Cliente cliente) {
+		Long id = clientes.size() > 0 ? clientes.lastKey() + 1L : 1L;
+
+		cliente.setId(id);
+
+		clientes.put(cliente.getId(), cliente);
+		
+		return cliente;
+	}
+
+	@Override
+	public Cliente modificar(Cliente cliente) {
+		clientes.put(cliente.getId(), cliente);
+		return cliente;
+	}
+
+	@Override
+	public void borrar(Long id) {
+		if(clientes.containsKey(id)) {
+			clientes.remove(id);
+		} else {
+			throw new AccesoDatosException("No se ha encontrado el registro a borrar");
+		}
+	}
+
+}
