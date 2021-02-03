ab5a222860366d616263b5ead80d12d074c537ef
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-03 10:54:01 +0100

    formatado

--- servidorrestservlets/src/com/mkss/formacion/rest/servidoresrestservlets/controlador/entidades/Cliente.java
+++ servidorrestservlets/src/com/mkss/formacion/rest/servidoresrestservlets/controlador/entidades/Cliente.java
@@ -2,11 +2,9 @@ package com.mkss.formacion.rest.servidoresrestservlets.controlador.entidades;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
