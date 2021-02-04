06c905a25565e529986890a92bd6e2aeec1bc73d
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-04 18:25:23 +0100

    falto poner el @manyToOne muchos a uno

--- Aplicacion-Api-Rest-Spring-uno-a-varios/src/main/java/com/mkss/formacion/aplicacion/entity/Producto.java
+++ Aplicacion-Api-Rest-Spring-uno-a-varios/src/main/java/com/mkss/formacion/aplicacion/entity/Producto.java
@@ -2,31 +2,23 @@ package com.mkss.formacion.aplicacion.entity;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name="productos")
-@Data @AllArgsConstructor @NoArgsConstructor
-public class Producto {
+@Table(name = "productos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
+public class Producto {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private BigDecimal precio;
 	
-	@ManyToOne //(fetch = FetchType.EAGER) me trae todos los datos del producto y categoria
+	@ManyToOne //(fetch = FetchType.EAGER)
 	private Categoria categoria;
-
-	
-	
 }
