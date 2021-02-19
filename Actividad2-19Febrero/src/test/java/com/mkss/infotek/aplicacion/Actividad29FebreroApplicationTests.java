0010747b5c39680bbe59b3f7bad1521bd8a1c091
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-19 12:15:42 +0100

    setup del proyecto

---
+++ Actividad2-19Febrero/src/test/java/com/mkss/infotek/aplicacion/Actividad29FebreroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkss.infotek.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad29FebreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
